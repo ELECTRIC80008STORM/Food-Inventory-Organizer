5c0c46de65008859a901b63a130b2b5297928c4f
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2022-09-02 23:16:29 -0500

    Add files via upload
    No logré conseguir que funcionará la librería "plyer" para lograr que funcionaran las notificaciones, y tuvé complicaciones para crear la clase al meter datos de fecha. Todas estas pruebas las realicé de manera externa en otro archivo e intentaré que puedan ya implementarse para la siguiente entrega, o al menos buscaré otra forma de resolver los problemas.

--- Nombre Sin Decidir.py
+++ Nombre Sin Decidir.py
@@ -10,24 +10,47 @@ Enviando notificaciones al usuario cuando se requiera que recuerde algo.
 
 #Funciones (Mover después a un archivo aparte)
 
-#Convertir en clase en algún futuro
-alimento = str((input("Ingresa un alimento: ")))
-lista_de_alimentos = []
-lista_de_alimentos.append(alimento.capitalize())
 
-print("Alimentos actuales: ", *lista_de_alimentos)
+"""Programa Principal (No logré conseguir que las notificaciones funcionaran en 
+la prueba externa que realicé ni logré convertir las funciones a una clase, por ahorita) """
+lista_de_alimentos = []
+lista_de_su_caducidad = []
+alimento_numero = {}
+
+
+def ingresar_alimento():
+    alimento = str((input("Ingresa un alimento: ")))
+    caducidad = str(input("Y su caducidad como dd/mm/aaaa: "))
+    lista_de_alimentos.append(alimento.capitalize())
+    numero_alimento = lista_de_alimentos.index(alimento.capitalize())
+    alimento_numero[alimento.capitalize()] = numero_alimento
+    lista_de_su_caducidad.append(caducidad)
+    
+
+ingresar_alimento()
+
+def mostrar_datos_alimentos():
+    print("Alimentos actuales: ")
+    print(*lista_de_alimentos, sep = ", ")
+
+def mostrar_datos_caducidad():
+    print("Caducidad: ")
+    print(*lista_de_su_caducidad, sep = ", ")
+
+
+    
+print("Alimentos actuales: ", *lista_de_alimentos, "\nCaducidad: ", *lista_de_su_caducidad)
 
 respuesta_del_usuario_añadir_alimento = str(input("¿Quieres añadir otro alimento? "))
 
 def respuesta(respuesta_dada_pregunta):
     if respuesta_dada_pregunta.lower() == "si" or respuesta_dada_pregunta.lower() == "sí":
-        alimento = str((input("\nIngresa otro alimento: ")))
-        lista_de_alimentos.append(alimento.capitalize())
-        print("\nAlimentos actuales: ")
-        print(*lista_de_alimentos, sep = ", ")
+        ingresar_alimento()
+        mostrar_datos_alimentos()
+        mostrar_datos_caducidad()
         respuesta_dada_pregunta = str(input("\n¿Quieres añadir otro alimento? "))
         respuesta(respuesta_dada_pregunta)
-    if respuesta_dada_pregunta.lower() == "no":
+    elif respuesta_dada_pregunta.lower() == "no":
         print("\nEspero la app te haya sido útil ")
     else:
         print("No entendí tu respuesta", "\n¿Quieres ingresar otro alimento?")
