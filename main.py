976569ac6660d691f2ee2dffd8cbe07fce5e79b0
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2022-10-11 13:35:15 -0500

    Archivo Principal Finalizado

--- main.py
+++ main.py
@@ -1,61 +1,28 @@
 """
-Nombre-por-Decidir
-Organizador de inventario para personas foráneas.
-El programa recibirá un nombre de artículo e información relacionada al mismo y lo listará.
-Enviando notificaciones al usuario cuando se requiera que recuerde algo.
+Food Inventory Organizer
+Organizador de Alimentos para la despensa.
+El objetivo del programa es ayudar a tener una mejor organización y control sobre nuestros alimentos, pudiendo tener en un solo lugar su fecha como caducidad, la categoría que le ponemos y cuando creemos que se puede acabar.
 """
 
-#Bibliotecas (Si se requiere mover a un archivo aparte)
+#Biblioteca
+from time import sleep
+import classes_and_functions 
 
+#Listas Auxiliares
+lista_alimentos = []
 
-#Funciones (Mover después a un archivo aparte)
+#Código Principal
+def inicio_app():
+    """
+    Realiza las acciones iniciales al momento de empezar la app.
+    No poseé ningún parámetro.
+    No regresa ningún valor.
+    """
+    print("\nBienvenido!!!", "\nPruebe a ingresar su primer alimento:")
+    sleep(1)
+    classes_and_functions.crear_alimento(lista_alimentos)
+    classes_and_functions.mostrar_alimentos(lista_alimentos)
+    sleep(1.5)
+    classes_and_functions.opciones(lista_alimentos)
 
-
-"""Programa Principal (No logré conseguir que las notificaciones funcionaran en 
-la prueba externa que realicé ni logré convertir las funciones a una clase, por ahorita) """
-lista_de_alimentos = []
-lista_de_su_caducidad = []
-alimento_numero = {}
-
-
-def ingresar_alimento():
-    alimento = str((input("Ingresa un alimento: ")))
-    caducidad = str(input("Y su caducidad como dd/mm/aaaa: "))
-    lista_de_alimentos.append(alimento.capitalize())
-    numero_alimento = lista_de_alimentos.index(alimento.capitalize())
-    alimento_numero[alimento.capitalize()] = numero_alimento
-    lista_de_su_caducidad.append(caducidad)
-    
-
-ingresar_alimento()
-
-def mostrar_datos_alimentos():
-    print("Alimentos actuales: ")
-    print(*lista_de_alimentos, sep = ", ")
-
-def mostrar_datos_caducidad():
-    print("Caducidad: ")
-    print(*lista_de_su_caducidad, sep = ", ")
-
-
-    
-print("Alimentos actuales: ", *lista_de_alimentos, "\nCaducidad: ", *lista_de_su_caducidad)
-
-respuesta_del_usuario_añadir_alimento = str(input("¿Quieres añadir otro alimento? "))
-
-def respuesta(respuesta_dada_pregunta):
-    if respuesta_dada_pregunta.lower() == "si" or respuesta_dada_pregunta.lower() == "sí":
-        ingresar_alimento()
-        mostrar_datos_alimentos()
-        mostrar_datos_caducidad()
-        respuesta_dada_pregunta = str(input("\n¿Quieres añadir otro alimento? "))
-        respuesta(respuesta_dada_pregunta)
-    elif respuesta_dada_pregunta.lower() == "no":
-        print("\nEspero la app te haya sido útil ")
-    else:
-        print("No entendí tu respuesta", "\n¿Quieres ingresar otro alimento?")
-        respuesta_dada_pregunta = input()
-        respuesta(respuesta_dada_pregunta)
-
-
-respuesta(respuesta_del_usuario_añadir_alimento)
+inicio_app()
